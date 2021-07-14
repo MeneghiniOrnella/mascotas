54db1fa34be8ba5de569c70f6f63a6851c4052aa
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-14 16:21:32 -0300

    v 1.1

--- proyecto_grupal/mascota.sql
+++ proyecto_grupal/mascota.sql
@@ -1,1 +1,10 @@
-CREATE DATATABLE 
+CREATE DATABASE createAvatar;
+
+CREATE TABLE 'createAvatar' (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(30) NULL,
+  `genero` VARCHAR(10) NULL,
+  'edad' NUMERIC(2) NULL,
+  `color` VARCHAR(30) NULL,
+  PRIMARY KEY (`id`)) ;
+
