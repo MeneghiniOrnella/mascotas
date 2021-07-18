8d3d6e96e13cedcb3fea63cd3ad58553e1afba6c
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-18 15:59:41 -0300

    v 1.7

--- proyecto_grupal/app.py
+++ proyecto_grupal/app.py
@@ -42,6 +42,11 @@ def storage():
     _tel=request.form['tel']
     _estado=request.form['estado']
     _foto=request.flies['foto']
+    now=datetime.now()
+    tiempo=now.strftime("%Y%H%M%S")
+    if _foto.filename='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
     _fechaFin=request.form['fechaFin']
     sql ="INSERT INTO `pacientesvet` (`nombre_mascota`, `id_mascota`, `especie`, `raza`, `tamaño`, `peso_actual`, `color`, `genero`, `fecha_nac`, `estado`, `vacunas_dadas`, `nombre_dueño`, `apellido_dueño`, `direccion`, `telefono`, `fecha_defuncion`) VALUES (%s, NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
     datos=(_nombre,_id,_especie,_raza,_tamaño,_genero,_peso,_color,_fechaNac,_nombreDueño,_apellidoDueño,_direccion,_tel,_estado,_foto.filename,_fechaFin)
