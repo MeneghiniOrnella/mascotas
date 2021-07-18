9b6b4ba36c7ff1e6f5d50485459b36e7b6cca682
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-18 15:43:50 -0300

    v 1.7

--- proyecto_grupal/app.py
+++ proyecto_grupal/app.py
@@ -40,14 +40,15 @@ def storage():
     _direccion=request.form['direccion']
     _tel=request.form['tel']
     _estado=request.form['estado']
-    _foto=request.form['foto']
+    _foto=request.flies['foto']
     _fechaFin=request.form['fechaFin']
-    sql ="INSERT INTO `pacientesvet` (`nombre_mascota`, `id_mascota`, `especie`, `raza`, `tamaño`, `peso_actual`, `color`, `genero`, `fecha_nac`, `estado`, `vacunas_dadas`, `nombre_dueño`, `apellido_dueño`, `direccion`, `telefono`, `fecha_defuncion`) VALUES ('Luna', '1', 'perro', '', 'grande', '29', 'dorado', 'hembra', '2018-02-14', 'sano', '15', 'adriana', 'adriluna', 'Luna 1200, CABA', '1122558855', '');"
+    sql ="INSERT INTO `pacientesvet` (`nombre_mascota`, `id_mascota`, `especie`, `raza`, `tamaño`, `peso_actual`, `color`, `genero`, `fecha_nac`, `estado`, `vacunas_dadas`, `nombre_dueño`, `apellido_dueño`, `direccion`, `telefono`, `fecha_defuncion`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+    datos=(_nombre,_id,_especie,_raza,_tamaño,_genero,_peso,_color,_fechaNac,_nombreDueño,_apellidoDueño,_direccion,_tel,_estado,_foto.filename,_fechaFin)
     conn= mysql.connect()
     cursor= conn.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql,datos)
     conn.commit()
-    return render_template('templates\index.html')
+    return render_template('templates/index.html')
 
 
 if __name__== '__main__':
