16c921a79b70c2b3684492a59735d767e0a33490
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-19 19:19:42 -0300

    v 1.7

--- proyectoGrupal/app.py
+++ proyectoGrupal/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template #ELIMINAR REQUEST POR AHORA
+from flask import render_template,request,redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +12,9 @@ app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema'
 mysql.init_app(app)
+
+CARPETA= os.path.join('uploads')
+app.config['CARPETA']=CARPETA
 
 @app.route('/')
 def index():
@@ -60,9 +64,9 @@ def update():
     _fechaFin=request.form['fechaFin']
     id=request.form['idForm']
 
-    sql ="INSERT INTO `pacientesvet` (`nombre_mascota`, `id_mascota`, `especie`, `raza`, `tamaño`, `peso_actual`, `color`, `genero`, `fecha_nac`, `estado`, `vacunas_dadas`, `nombre_dueño`, `apellido_dueño`, `direccion`, `telefono`, `fecha_defuncion`) VALUES (%s, NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+    sql ="UPDATE `pacientesvet`SET `nombre_mascota`=%s, `id_mascota`=%s, `especie`=%s, `raza`=%s, `tamaño`=%s, `peso_actual`=%s, `color`=%s, `genero`=%s, `fecha_nac`=%s, `estado`=%s, `vacunas_dadas`=%s, `nombre_dueño`=%s, `apellido_dueño`=%s, `direccion`=%s, `telefono`=%s,WHERE `fecha_defuncion`=%s;"
     
-    datos=(_nombre,_id,_especie,_raza,_tamaño,_genero,_peso,_color,_fechaNac,_nombreDueño,_apellidoDueño,_direccion,_tel,_estado,_foto.filename,_fechaFin)
+    datos=(_nombre,_id,_especie,_raza,_tamaño,_genero,_peso,_color,_fechaNac,_nombreDueño,_apellidoDueño,_direccion,_tel,_estado,id,_fechaFin)
     conn= mysql.connect()
     cursor= conn.cursor()
     cursor.execute(sql,datos)
