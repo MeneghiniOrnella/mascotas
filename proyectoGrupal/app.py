f8c0f1a64819da771f18ea4f8d693dde381dfd6d
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-20 11:26:57 -0300

    v 1.7

--- proyectoGrupal/app.py
+++ proyectoGrupal/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template,request,redirect
 from flaskext.mysql import MySQL
+from flask import send_from_directory
 from datetime import datetime
 import os
 
