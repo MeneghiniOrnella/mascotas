78e3a3e43491fe5fd5a659a1a7f3e141f4bc455c
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-20 12:05:13 -0300

    v 1.7

--- proyectoGrupal/app.py
+++ proyectoGrupal/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template,request,redirect
+from flask import render_template,request,redirect,url_for
 from flaskext.mysql import MySQL
 from flask import send_from_directory
 from datetime import datetime
@@ -19,7 +19,7 @@ app.config['CARPETA']=CARPETA
 
 @app.route("/uploads/<nombreFoto>")
 def uploads(nombreFoto):
-    
+    return send_from_directory(app.config['CARPETA'],nombreFoto)
 
 @app.route('/')
 def index():
