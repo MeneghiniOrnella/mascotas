601b6beef78b9fed92a5d39701829bd535e7d9f5
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-21 17:33:58 -0300

    v 1.8

--- templates/app.py
+++ templates/app.py
@@ -12,7 +12,7 @@ mysql= MySQL()
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_DB']='pacientesvet'
+app.config['MYSQL_DATABASE_Db']='pacientesvet'
 mysql.init_app(app)
 
 CARPETA= os.path.join('uploads')
@@ -20,18 +20,58 @@ app.config['CARPETA']=CARPETA
 
 @app.route('/')
 def index():
-    sql ="INSERT INTO `pacientesvet` (`nombre_mascota`, `id_mascota`, `especie`, `raza`, `tamaño`, `peso_actual`, `color`, `genero`, `fecha_nac`, `estado`, `vacunas_dadas`, `nombre_dueño`, `apellido_dueño`, `direccion`, `telefono`) VALUES ('Luna', '1', 'perro', '.', 'grande', '29', 'dorado', 'hembra', '2018-02-14', 'sano', '15', 'adriana', 'adriluna', 'Luna 1200, CABA', '1122558855');"
+    sql="SELECT * FROM `pacientesvet`.`pacientes`;"
+    '''sql ="INSERT INTO `pacientesvet` (`nombre_mascota`, `id_mascota`, `especie`, `raza`, `tamaño`, `peso_actual`, `color`, `genero`, `fecha_nac`, `estado`, `vacunas_dadas`, `nombre_dueño`, `apellido_dueño`, `direccion`, `telefono`) VALUES ('Luna', '1', 'perro', '.', 'grande', '29', 'dorado', 'hembra', '2018-02-14', 'sano', '15', 'adriana', 'adriluna', 'Luna 1200, CABA', '1122558855');"'''
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    pacientes=cursor.fetchall() 
+    print (pacientes)
+    conn.commit()
+    return render_template('empleados/index.html',pacientes=pacientes)
+
+@app.route("/create")
+def create():
+        return render_template('pacientes/create.html')
+
+@app.route("/store, methods=['POST']")
+def storage():
+    _nombre=request.form['nombreForm']
+    _id=request.form['idForm']
+    _especie=request.form['especie']
+    _raza=request.form['raza']
+    _tamano=request.form['tamano']
+    _genero=request.form['genero']
+    _peso=request.form['peso']
+    _color=request.form['colorForm']
+    _fechaNac=request.form['fechaNac']
+    _nombreDueno=request.form['nombreDueno']
+    _apellidoDueno=request.form['apellidoDueno']
+    _direccion=request.form['direccion']
+    _tel=request.form['telForm']
+    _estado=request.form['estado']
+    _foto=request.files['foto']
+    now=datetime.now()
+    tiempo=now.strftime("%Y%H%M%S")
+    if _nombre=='' or _especie=='':
+        flash('Recuerda llenar estos campos')
+        return redirect(url_for('create'))
+    if _foto.filename=='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)    
+    sql="INSERT INTO `pacientesvet` (`nombreMascota`, `idMascota`, `especie`, `raza`, `tamano`, `peso_actual`, `color`, `genero`, `fechaNac`, `estado`, `nombreDueno`, `apellidoDueno`, `direccion`, `telefono`,`foto`) VALUES (%s,NULL,%s,%s,%s, %s, %s, %s,%s,%s,%s,%s,%s,%s)"
+    datos=(_nombre,_id,_especie,_raza,_tamano,_genero,_peso,_color,_fechaNac,_nombreDueno,_apellidoDueno,_direccion,_tel,_estado,_foto.filename)
     conn= mysql.connect()
     cursor= conn.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql,datos)
     conn.commit()
     return render_template('pacientes/index.html')
 
-@app.route("/uploads/<nombreFoto>")
+@app.route('uploads/<nombreFoto>')
 def uploads(nombreFoto):
     return send_from_directory(app.config['CARPETA'],nombreFoto)
 
-@app.route('/destroy/<int:id>')
+@app.route('destroy/<int:id>')
 def destroy(id):
     conn= mysql.connect()
     cursor= conn.cursor()
@@ -94,49 +134,6 @@ def update():
         conn.commit()
 
     return redirect('/')
-
-@app.route("/create")
-def create():
-    return render_template('pacientes/create.html')
-
-@app.route("/connect, methods=['POST']")
-def storage():
-    _nombre=request.form['nombreForm']
-    _id=request.form['idForm']
-    _especie=request.form['especie']
-    _raza=request.form['raza']
-    _tamano=request.form['tamano']
-    _genero=request.form['genero']
-    _peso=request.form['peso']
-    _color=request.form['colorForm']
-    _fechaNac=request.form['fechaNac']
-    _nombreDueno=request.form['nombreDueno']
-    _apellidoDueno=request.form['apellidoDueno']
-    _direccion=request.form['direccion']
-    _tel=request.form['tel']
-    _estado=request.form['estado']
-    _foto=request.flies['foto']
-    # now=datetime.now()
-    # tiempo=now.strftime("%Y%H%M%S")
-
-    '''if _nombre='' or _especie='':
-        flash('Recuerda llenar estos campos')
-        return redirect(url_for('create'))'''
-
-    '''if _foto.filename='':
-        nuevoNombreFoto=tiempo+_foto.filename
-        _foto.save("uploads/"+nuevoNombreFoto)'''
-    
-    sql="INSERT INTO `pacientesvet` (`nombreMascota`, `idMascota`, `especie`, `raza`, `tamano`, `peso_actual`, `color`, `genero`, `fechaNac`, `estado`, `nombreDueno`, `apellidoDueno`, `direccion`, `telefono`) VALUES (%s,NULL,%s,%s,%s, %s, %s, %s,%s,%s,%s,%s,%s,%s)"
-    
-    datos=(_nombre,_id,_especie,_raza,_tamano,_genero,_peso,_color,_fechaNac,_nombreDueno,_apellidoDueno,_direccion,_tel,_estado,_foto.filename)
-    conn= mysql.connect()
-    cursor= conn.cursor()
-    cursor.execute(sql)
-    mascotas=cursor.fetchall()
-    print(mascotas)
-    conn.commit()
-    return render_template('')
 
 if __name__=='__main__':
     app.run(debug=True)
