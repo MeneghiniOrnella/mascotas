fec305e5d658ebfec0f6571a7419f9ea25b0e631
MeneghiniOrnella <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-07-22 00:43:38 -0300

    v 1.8

--- app.py
+++ app.py
@@ -28,6 +28,14 @@ def index():
     print (mascotitas)
     conn.commit()
     return render_template('pacientes/index.html',mascotitas=mascotitas)
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    sql="DELETE FROM `pacientesvet`.`pacientesvet` WHERE id=%s;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,(id))
+    conn.commit()
+    return redirect('/')
 
 @app.route('/edit', methods=["GET"])
 def edit():
@@ -80,19 +88,6 @@ def storage():
 def uploads(nombreFoto):
     return send_from_directory(app.config['CARPETA'],nombreFoto)
 '''
-'''@app.route('destroy/<int:id>')
-def destroy(id):
-    conn= mysql.connect()
-    cursor= conn.cursor()
-    
-    cursor.execute("SELECT foto FROM pacientesvet WHERE id=%s", id)
-    fila=cursor.fetchall()
-    os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
-    
-    cursor.execute("DELETE FROM pacientesvet WHERE id=%s", (id))
-    conn.commit()
-    return redirect('/')'''
-
 @app.route('/update', methods=['POST'])
 def update():
     _nombre=request.form['nombreForm']
@@ -104,22 +99,19 @@ def update():
     _peso=request.form['peso']
     _color=request.form['colorForm']
     _fechaNac=request.form['fechaNac']
-    _nombreDueno=request.form['nombreDueño']
-    _apellidoDueno=request.form['apellidoDueño']
+    _nombreDueno=request.form['nombreDueno']
+    _apellidoDueno=request.form['apellidoDueno']
     _direccion=request.form['direccion']
-    _tel=request.form['tel']
+    _tel=request.form['telefono']
     _estado=request.form['estado']
     _foto=request.flies['foto']
     id=request.form['idForm']
-
     sql ="UPDATE `pacientesvet`SET `nombreMascota`=%s, `idMascota`=%s, `especie`=%s, `raza`=%s, `tamano`=%s, `peso`=%s, `color`=%s, `genero`=%s, `fechaNac`=%s, `estado`=%s, `nombreDueno`=%s, `apellidoDueno`=%s, `direccion`=%s,WHERE `telefono`=%s;"
-    
     datos=(_nombre,_id,_especie,_raza,_tamano,_genero,_peso,_color,_fechaNac,_nombreDueno,_apellidoDueno,_direccion,_tel,_estado,id)
     conn= mysql.connect()
     cursor= conn.cursor()
     cursor.execute(sql,datos)
     conn.commit()
-
     now=datetime.now()
     tiempo=now.strftime("%Y%H%M%S")
     if _foto.filename=='':
@@ -127,11 +119,9 @@ def update():
         _foto.save("uploads/"+nuevoNombreFoto)
         cursor.execute("SELECT foto FROM pacientes_mascotas WHERE id=%s", id)
         fila= cursor.fetchall()
-
         os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
         cursor.execute("UPDATE pacientes_mascotas SET foto=%s WHERE id=%s",(nuevoNombreFoto,id))
         conn.commit()
-
     return redirect('/')
 
 if __name__=='__main__':
